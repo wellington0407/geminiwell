6330c6801dcd53f5256acff82a28134a7cc09187
Wellington Francisco Benicio <wellington0407@gmail.com>
2024-05-16 09:46:30 -0300

    melhorarndo projeto

---
+++ geminiwell.py
@@ -0,0 +1,48 @@
+import os
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, responses
+from starlette.staticfiles import StaticFiles
+import json
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ.get('GENAI_API_KEY'))
+
+model = genai.GenerativeModel('gemini-pro')
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+connections = []
+
+@app.get("/")
+async def get():
+    with open("templates/index.html", "r") as file:
+        html_content = file.read()
+    return responses.HTMLResponse(content=html_content)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connections.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            if data.strip():
+                prompt = f"""
+                Aborde essa pergunta em um texto curtos de duas linhas no maximo com a sensibilidade de uma terapeuta ou psicóloga, faça parecer uma conversa. segue a pergunta: {data}.          
+                """
+                response = model.generate_content(prompt)
+                text_response = response.candidates[0].content.parts[0].text
+                await websocket.send_text(text_response)
+    except WebSocketDisconnect:
+        connections.remove(websocket)
+        await websocket.close()
+
+
+
+# Executar o aplicativo FastAPI
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ static/script/script.js
@@ -0,0 +1,38 @@
+var ws = new WebSocket("ws://" + window.location.host + "/ws");
+
+ws.onmessage = function(event) {
+    var chatHistory = document.getElementById("chat-history");
+    var message = document.createElement("div");
+    var decodedText = decodeURIComponent(event.data);
+    message.innerText = "GeminiWell: " + decodedText;
+    chatHistory.appendChild(message);
+};
+
+
+function sendMessage() {
+    var userInput = document.getElementById("user-input").value;
+    var chatHistory = document.getElementById("chat-history");
+    var message = document.createElement("div");
+    message.innerText = "Você: " + userInput;
+    chatHistory.appendChild(message);
+    ws.send(userInput);
+    document.getElementById("user-input").value = "";
+}
+
+document.getElementById("send-button").addEventListener("click", sendMessage);
+
+document.getElementById("clear-button").addEventListener("click", function() {
+    document.getElementById('chat-history').innerHTML = '';
+});
+
+document.getElementById("hug-button").addEventListener("click", function() {
+    var chatHistory = document.getElementById("chat-history");
+    var message = document.createElement("div");
+    message.innerText = "Você é importante e merece todo amor do mundo! 🤗";
+    chatHistory.appendChild(message);
+})
+document.getElementById("user-input").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
